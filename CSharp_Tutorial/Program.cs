b804a43909193dbfee8e2c88b78b2a34069d3f05
Afiq Ara <Afiq Ara@LKL-SNAPDRAGON1>
2026-03-19 16:15:25 +0800

    Add project files.

---
+++ CSharp_Tutorial/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp_Tutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
